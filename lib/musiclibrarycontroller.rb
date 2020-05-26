525c1626edfd7117f167048b011ea4ed5182e84c
Becky Zorn <bczorn@gmail.com>
2020-05-26 04:47:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -102,7 +102,7 @@ class MusicLibraryController
     input = gets.strip
     input.to_i
     j = Song.all.length
-    if input.between?(1,j)
+    for input.between?(1,j)
       songs = Song.all.sort{|a,b| a.name <=> b.name}
       x = songs.fetch(input)
       puts "Playing #{x.artist.name} by #{x.name}"
